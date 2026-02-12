602a84f489ddb9e9b28137c8089bbb731dcd3c20
Shin <shinjpn1@gmail.com>
2026-02-12 20:32:51 +0900

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="auth2fa",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     package_data={
         "auth2fa": ["sql/**/*.sql"],
